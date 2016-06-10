8ac7bf32d3064677ccab0ffc9cadbfde20d132ac
Jan Peter Alexander Rajagukguk <jpmrblood@gmail.com>
2016-06-10 10:17:46 +0700

    Create a new module and move all the source code and build script to that new module.

---
+++ percuma-app/src/main/java/aja/jp/percuma/TestMain.java
@@ -0,0 +1,7 @@
+package aja.jp.percuma;
+ 
+class TestMain {
+  public static void main (String ... args) {
+    java.util.logging.Logger.getLogger(TestMain.class.getName()).info("Hello, world!");
+  }
+}
