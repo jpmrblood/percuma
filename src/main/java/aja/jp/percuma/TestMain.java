05acc554a7ecfec688c9900d7800df352f5bd759
Jan Peter Alexander Rajagukguk <jpmrblood@gmail.com>
2016-06-10 14:20:59 +0700

    fix .gitignore

--- src/main/java/aja/jp/percuma/TestMain.java
+++
@@ -1,7 +0,0 @@
-package aja.jp.percuma;
- 
-class TestMain {
-  public static void main (String ... args) {
-    java.util.logging.Logger.getLogger(TestMain.class.getName()).info("Hello, world!");
-  }
-}
